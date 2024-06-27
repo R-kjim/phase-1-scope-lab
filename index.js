a3daae78269f545f0fcd2eca65dc06518479ada9
R-kjim <rkayjim@gmail.com>
2024-06-27 16:55:06 +0300

    understanding scope

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+//declare a variable and create a function that returns that variable as an uppercase 
+var customerName='bob'
+function upperCaseCustomerName(){
+    customerName='bob'.toUpperCase()
+}
+
+//function that declares a variable in global scope
+function setBestCustomer(){
+    bestCustomer='not bob'
+}
+
+//function that overwrites a previously declared variable created in a function scope
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+}
+
+//unsuccessfully trying to change a blobal variable declaredusing const
+const leastFavoriteCustomer='mary'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='john'
+}
